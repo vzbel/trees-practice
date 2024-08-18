86ad445dfcacbe8e54ac21b45486c883afd5ef66
vzbel <142253332+vzbel@users.noreply.github.com>
2024-08-18 11:52:09 -0600

    Parameterize generic BST

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 import BinarySearchTree.BST;
-import Node.BSTNode;
 
 public class Main {
     public static void main(String[] args) {
-        BST<Integer> tree = new BST();
+        BST<Integer> tree = new BST<>();
     }
 }
